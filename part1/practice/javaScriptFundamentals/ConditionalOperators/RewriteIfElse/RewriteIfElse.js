b0f97e719285da1d47a126fea0b6750734b5db4f
mi5ha6in <mi5ha6in@gmail.com>
2017-10-06 14:27:01 +0500

    Completed the task: Rewrite if..else

---
+++ part1/practice/javaScriptFundamentals/ConditionalOperators/RewriteIfElse/RewriteIfElse.js
@@ -0,0 +1,4 @@
+let message = prompt("Enter your login:", "");
+(message == "Employee") ? alert("Hello") :
+(message == "Director") ? alert("Greetings") :
+(message == "") ? alert("No login") : alert("Canceled.")
