89095157e5c3d31303a3c314a885be581168445d
mi5ha6in <mi5ha6in@gmail.com>
2017-10-07 13:08:29 +0500

    Completed the task: Check the range between

---
+++ part1/practice/javaScriptFundamentals/LogicalOperators/CheckTheRangeBetween/CheckTheRangeBetween.js
@@ -0,0 +1,7 @@
+let age = prompt("Enter age", "");
+if (age >= 14 && age <= 90) {
+  alert(`The age ${age} years old is between 14 and 90`);
+}
+else {
+  alert(`The age ${age} years old isn't between 14 and 90`);
+}
