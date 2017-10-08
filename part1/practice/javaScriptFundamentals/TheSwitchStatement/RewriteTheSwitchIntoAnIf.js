2da7fa21a223a30b08ee481799c6be34a508e449
mi5ha6in <mi5ha6in@gmail.com>
2017-10-08 15:50:09 +0500

    Completed the task: Rewrite the switch into an if

---
+++ part1/practice/javaScriptFundamentals/TheSwitchStatement/RewriteTheSwitchIntoAnIf.js
@@ -0,0 +1,10 @@
+let browser = "Chrome";
+if (browser == "Edge") {
+  console.log("You've got the Edge!");
+}
+else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") {
+  console.log("Okay we support these browsers too");
+}
+else {
+  console.log("We hope that this page looks ok!");
+}
